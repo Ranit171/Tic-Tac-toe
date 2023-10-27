b9d20fefc0c594028b95bd0ef47af8540199cab2
Ranit Basak <branit764@gmail.com>
2023-10-28 00:16:44 +0530

    Bugs fixed

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const boxes = document.querySelectorAll(".box");
 const gameInfo = dociment.querySelector(".game-info");
 const newGameBtn = dociment.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winniPositions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// function to intialize the game
+function initGame() {
+  currentPlayer = "x";
+  gameGrid = ["", "", "", "", "", "", "", "", ""];
+  newGameBtn.classList.remove("active");
+}
